86f07f74652e71936e4c22163984f996f120faea
amalkochuparambilp <98939651+amalkochuparambilp@users.noreply.github.com>
2025-03-10 20:04:08 +0000

    animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './Hero.css'
+import GradientText from '../Animations/GradientText/GradientText'
 
 function Hero() {
   return (
     <div className='hero'>
         <div className='hero-text'>
-            <h1>Anarkali Boutique</h1>
+            <GradientText
+                colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+                animationSpeed={3}
+                showBorder={false}
+                className="custom-class"
+            >
+              <h1>Anarkali Boutique</h1>
+            </GradientText>
             <p>Designs of unique style</p>
             <button className='btn'>
                 <a href='https://instagram.com/amalkochuparambilp'>Explore More</a>
